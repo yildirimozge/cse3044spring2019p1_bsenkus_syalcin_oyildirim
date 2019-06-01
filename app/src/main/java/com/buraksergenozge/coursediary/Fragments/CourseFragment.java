30a248ed20e78f8d4d35b96e20c511e82498133a
yildirimozge <47923719+yildirimozge@users.noreply.github.com>
2019-06-01 23:07:51 +0300

    Update CourseFragment.java

--- app/src/main/java/com/buraksergenozge/coursediary/Fragments/CourseFragment.java
+++ app/src/main/java/com/buraksergenozge/coursediary/Fragments/CourseFragment.java
@@ -1,17 +1,16 @@
 package com.buraksergenozge.coursediary.Fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
+import android.support.v7.app.AlertDialog;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,17 +18,19 @@ import android.widget.Toast;
 import com.buraksergenozge.coursediary.Activities.MainScreen;
 import com.buraksergenozge.coursediary.Data.Assignment;
 import com.buraksergenozge.coursediary.Data.Course;
-import com.buraksergenozge.coursediary.Data.CourseDiaryDB;
 import com.buraksergenozge.coursediary.Data.CourseHour;
 import com.buraksergenozge.coursediary.Data.User;
 import com.buraksergenozge.coursediary.ListAdapter;
 import com.buraksergenozge.coursediary.R;
 
-public class CourseFragment extends ListFragment implements AdapterView.OnItemClickListener {
+import java.util.Collections;
+
+public class CourseFragment extends ListFragment implements AdapterView.OnItemClickListener, View.OnClickListener {
     private static final String ARG_PARAM1 = "courseID";
     public Course course;
+    private LinearLayout courseHourListHeader, assignmentListHeader;
     private ListView courseHourListView, assignmentListView;
-    private TextView emptyCourseTV;
+    private TextView emptyCourseHourList_TV, emptyAssignmentList_TV;
 
     public CourseFragment() {
     }
@@ -62,33 +63,45 @@ public class CourseFragment extends ListFragment implements AdapterView.OnItemCl
         super.onStart();
         TextView semesterTitleTV = getView().findViewById(R.id.courseTitle_TV);
         semesterTitleTV.setText(course.getName());
+        courseHourListHeader = getView().findViewById(R.id.courseHourListHeader);
+        courseHourListHeader.setOnClickListener(this);
         courseHourListView = getView().findViewById(R.id.courseHourListView);
         courseHourListView.setOnItemClickListener(this);
         registerForContextMenu(courseHourListView);
+        assignmentListHeader = getView().findViewById(R.id.assignmentListHeader);
+        assignmentListHeader.setOnClickListener(this);
         assignmentListView = getView().findViewById(R.id.assignmentListView);
         assignmentListView.setOnItemClickListener(this);
         registerForContextMenu(assignmentListView);
-
-        emptyCourseTV = getView().findViewById(R.id.emptyCourse_TV);
+        emptyCourseHourList_TV = getView().findViewById(R.id.emptyCourseHourList_TV);
+        emptyAssignmentList_TV = getView().findViewById(R.id.emptyAssignmentList_TV);
         updateView();
+        contextObject = course;
     }
 
     public void setVisibilities(boolean isCourseHourListEmpty, boolean isAssignmentListEmpty) {
-        if(assignmentListView != null && courseHourListView != null && emptyCourseTV != null) {
-            if (isCourseHourListEmpty && isAssignmentListEmpty) {
+        if(assignmentListView != null && courseHourListView != null && emptyCourseHourList_TV != null && emptyAssignmentList_TV != null) {
+            if (isCourseHourListEmpty) {
                 courseHourListView.setVisibility(View.GONE);
-                assignmentListView.setVisibility(View.GONE);
-                emptyCourseTV.setVisibility(View.VISIBLE);
-            } else if (emptyCourseTV.getVisibility() == View.VISIBLE) {
+                emptyCourseHourList_TV.setVisibility(View.VISIBLE);
+            }
+            else {
                 courseHourListView.setVisibility(View.VISIBLE);
+                emptyCourseHourList_TV.setVisibility(View.GONE);
+            }
+            if (isAssignmentListEmpty) {
+                assignmentListView.setVisibility(View.GONE);
+                emptyAssignmentList_TV.setVisibility(View.VISIBLE);
+            }
+            else {
                 assignmentListView.setVisibility(View.VISIBLE);
-                emptyCourseTV.setVisibility(View.GONE);
+                emptyAssignmentList_TV.setVisibility(View.GONE);
             }
         }
     }
 
     public boolean updateCourseHourList() {
-        //course.setCourseHours(CourseDiaryDB.getDBInstance(getActivity()).courseDAO().getAllCourseHoursOfCourse(course));
+        Collections.sort(course.getCourseHours());
         ListAdapter<CourseHour> adapter = new ListAdapter<>(getActivity(), course.getCourseHours());
         courseHourListView.setAdapter(adapter);
         ((BaseAdapter)courseHourListView.getAdapter()).notifyDataSetChanged();
@@ -96,7 +109,6 @@ public class CourseFragment extends ListFragment implements AdapterView.OnItemCl
     }
 
     public boolean updateAssignmentList() {
-        //course.setAssignments(CourseDiaryDB.getDBInstance(getActivity()).courseDAO().getAllAssignmentsOfCourse(course));
         ListAdapter<Assignment> adapter = new ListAdapter<>(getActivity(), course.getAssignments());
         assignmentListView.setAdapter(adapter);
         ((BaseAdapter)assignmentListView.getAdapter()).notifyDataSetChanged();
@@ -113,13 +125,13 @@ public class CourseFragment extends ListFragment implements AdapterView.OnItemCl
             }catch (NullPointerException ex) {
                 ex.printStackTrace();
                 return;
-            }/*
+            }
             FragmentTransaction fragTransaction = fragManager.beginTransaction();
             fragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            SemesterFragment semesterFragment = SemesterFragment.newInstance(semesterID);
-            fragTransaction.replace(R.id.mainArchiveLayout, semesterFragment,"semesterFragment");
+            CourseHourFragment courseHourFragment = CourseHourFragment.newInstance(courseHourID);
+            fragTransaction.replace(R.id.mainArchiveLayout, courseHourFragment,"courseHourFragment");
             fragTransaction.addToBackStack(null);
-            fragTransaction.commit();*/
+            fragTransaction.commit();
         }
         else if (adapterView == assignmentListView) {
             long assignmentID = ((Assignment)adapterView.getItemAtPosition(i)).getAssignmentID();
@@ -129,29 +141,35 @@ public class CourseFragment extends ListFragment implements AdapterView.OnItemCl
             }catch (NullPointerException ex) {
                 ex.printStackTrace();
                 return;
-            }/*
+            }
             FragmentTransaction fragTransaction = fragManager.beginTransaction();
             fragTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-            SemesterFragment semesterFragment = SemesterFragment.newInstance(semesterID);
-            fragTransaction.replace(R.id.mainArchiveLayout, semesterFragment,"semesterFragment");
+            AssignmentFragment assignmentFragment = AssignmentFragment.newInstance(assignmentID);
+            fragTransaction.replace(R.id.mainArchiveLayout, assignmentFragment,"assignmentFragment");
             fragTransaction.addToBackStack(null);
-            fragTransaction.commit();*/
+            fragTransaction.commit();
         }
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Object object = contextMenuSelectedListView.getAdapter().getItem(info.position);
+        final Object object = contextMenuSelectedListView.getAdapter().getItem(info.position);
         switch (item.getItemId()) {
             case R.id.floating_delete:
                 if (contextMenuSelectedListView == assignmentListView) {
-                    course.deleteAssignment(getContext(), (Assignment) object);
-                    ((MainScreen)getActivity()).onAppContentOperation("courseFragment", getString(R.string.assignment_deleted));
+                    new AlertDialog.Builder(getContext()).setMessage(getString(R.string.confirm_delete)).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    course.deleteAssignment(getContext(), (Assignment) object);
+                                    ((MainScreen)getActivity()).onAppContentOperation("courseFragment", getString(R.string.assignment_deleted));
+                                }}).setNegativeButton(R.string.no, null).show();
                 }
                 else if (contextMenuSelectedListView == courseHourListView) {
-//                    course.deleteCourseHour(getContext(), courseHour);
-//                    ((MainScreen)getActivity()).onAppContentOperation(getString(R.string.semester_deleted));
+                    new AlertDialog.Builder(getContext()).setMessage(getString(R.string.confirm_delete)).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            course.deleteCourseHour(getContext(), (CourseHour) object);
+                            ((MainScreen)getActivity()).onAppContentOperation("courseFragment", getString(R.string.course_hour_deleted));
+                        }}).setNegativeButton(R.string.no, null).show();
                 }
                 return true;
             case R.id.floating_info:
@@ -172,5 +190,23 @@ public class CourseFragment extends ListFragment implements AdapterView.OnItemCl
         boolean isCourseHourListEmpty = updateCourseHourList();
         boolean isAssignmentListEmpty = updateAssignmentList();
         setVisibilities(isCourseHourListEmpty, isAssignmentListEmpty);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.courseHourListHeader:
+                if (courseHourListView.getVisibility() == View.VISIBLE)
+                    courseHourListView.setVisibility(View.GONE);
+                else
+                    courseHourListView.setVisibility(View.VISIBLE);
+                break;
+            case R.id.assignmentListHeader:
+                if (assignmentListView.getVisibility() == View.VISIBLE)
+                    assignmentListView.setVisibility(View.GONE);
+                else
+                    assignmentListView.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
